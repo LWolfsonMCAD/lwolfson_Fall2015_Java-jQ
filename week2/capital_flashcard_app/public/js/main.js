308010c0f4ebc551bda882a143b3cf3efe26aee6
Lisa Wolfson <lwolfson@mcad.edu>
2015-09-04 22:07:20 -0500

    Currently works in Chrome, haven't tested in other browsers. Have it set to console.log vs alert for now…want to try and condense it somehow? JS seems redundant/clunky.

---
+++ week2/capital_flashcard_app/public/js/main.js
@@ -0,0 +1,39 @@
+var stateFlashcard = document.getElementById('alabama');
+var showCapital = function() {
+    console.log('The capital of Alabama is ' + document.getElementById('alabamaCapital').innerHTML + ".");
+};
+stateFlashcard.addEventListener('mouseover', showCapital);
+
+var stateFlashcard = document.getElementById('alaska');
+var showCapital = function() {
+    console.log('The capital of Alaska is ' + document.getElementById('alaskaCaptial').innerHTML + ".");
+};
+stateFlashcard.addEventListener('mouseover', showCapital);
+
+
+var stateFlashcard = document.getElementById('arizona');
+var showCapital = function() {
+    console.log('The capital of Arizona is ' + document.getElementById('arizonaCapital').innerHTML + ".");
+};
+stateFlashcard.addEventListener('mouseover', showCapital); 
+
+
+var stateFlashcard = document.getElementById('arkansas');
+var showCapital = function() {
+    console.log('The capital of Arkansas is ' + document.getElementById('arkansasCapital').innerHTML + ".");
+};
+stateFlashcard.addEventListener('mouseover', showCapital);
+
+
+var stateFlashcard = document.getElementById('california');
+var showCapital = function() {
+    console.log('The capital of California is ' + document.getElementById('californiaCapital').innerHTML + ".");
+};
+stateFlashcard.addEventListener('mouseover', showCapital);
+
+
+var stateFlashcard = document.getElementById('colorado');
+var showCapital = function() {
+    console.log('The capital of Colorado is ' + document.getElementById('coloradoCapital').innerHTML + ".");
+};
+stateFlashcard.addEventListener('mouseover', showCapital);
