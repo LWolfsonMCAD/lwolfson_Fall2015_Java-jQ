543bb8c890e1898125f87605d35471628d2c26cc
Lisa Wolfson <lwolfson@mcad.edu>
2015-09-20 15:36:15 -0500

    Had to add separate .js file to take care of IE8 quirks. Also removed trailing comma in both .js files to correct count in IE8.

--- week4/greatest_speeches/ie8.js
+++ week4/greatest_speeches/ie8.js
@@ -1,3 +1,8 @@
+/*
+IE8 can't handle the AddEventListener, so it's been replaced by attachEvent. Also had to change "click" to "onclick".
+
+*/
+
 //Speech metadata object constructor
 function Speech(speechAuthor, yearDelivered, beforeBCE) {
 	this.speechAuthor = speechAuthor;
@@ -20,26 +25,27 @@ var yearsBetweenSpeeches = ghandiYear - churchillYear;
 console.log("Ghandi's speech and Churchill's speech are " + yearsBetweenSpeeches + " years apart.");
 
 
-document.getElementById('BtnDonate').addEventListener('click', function(){
+document.getElementById('BtnDonate').attachEvent('onclick', function(){
   //Code in here executes when the user clicks the "Donate" button.
 
   	console.log("There are " + greatestSpeech.length + " speeches on the page.");
 });
 
-document.getElementById('BtnChurchill').addEventListener('click', function(){
+document.getElementById('BtnChurchill').attachEvent('onclick', function(){
   //Code in here executes when the user clicks the "Churchill" button.
 
   	console.log("This speech was written by " + greatestSpeech[0].speechAuthor + " in " + greatestSpeech[0].yearDelivered + ". It is " + greatestSpeech[0].beforeBCE + " that this year is B.C.E.");
 });
 
-document.getElementById('BtnGhandi').addEventListener('click', function(){
+document.getElementById('BtnGhandi').attachEvent('onclick', function(){
   //Code in here executes when the user clicks the "Ghandi" button.
 
   	console.log("This speech was written by " + greatestSpeech[1].speechAuthor + " in " + greatestSpeech[1].yearDelivered + ". It is " + greatestSpeech[1].beforeBCE + " that this year is B.C.E.");
 });
 
-document.getElementById('BtnDemosthenes').addEventListener('click', function(){
+document.getElementById('BtnDemosthenes').attachEvent('onclick', function(){
   //Code in here executes when the user clicks the "Demosthenes" button.
 
   	console.log("This speech was written by " + greatestSpeech[2].speechAuthor + " in " + greatestSpeech[2].yearDelivered + ". It is " + greatestSpeech[2].beforeBCE + " that this year is B.C.E.");
 });
+
