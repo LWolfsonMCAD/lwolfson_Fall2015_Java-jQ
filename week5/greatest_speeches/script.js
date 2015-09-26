a46c983d6e97f4566ffc1556d8d62343ec10640c
Lisa Wolfson <lwolfson@mcad.edu>
2015-09-25 21:23:57 -0500

    Worked on the assignment some more…have all of the code roughed in, need to see if it can be streamlined/cleaned up. Also need to test live version.

--- week5/greatest_speeches/script.js
+++ week5/greatest_speeches/script.js
@@ -29,8 +29,9 @@ else {
   console.log("Okay, I'll just call you User");
 }
 
+
 document.getElementById('BtnDonate').addEventListener('click', function(){
-  //Code in here executes when the user clicks the "Donate" button.
+  //Code in here executes when the user clicks the "Donate" button. Check to see who the user's favorite author is, then display how old the orator was when the speech was delivered.
   var favoriteAuthor = prompt(User + " who is your favorite author?");
 
   switch(favoriteAuthor.toLowerCase()) {
@@ -46,7 +47,7 @@ document.getElementById('BtnDonate').addEventListener('click', function(){
     console.log(speechesArray[2].author + " was " + speechesArray[2].authorAge + " during this speech.");
     break;
     default:
-    console.log("oops");
+    console.log("Please check the spelling of the name you entered and try again.");
     break;
   }
 
@@ -54,12 +55,68 @@ document.getElementById('BtnDonate').addEventListener('click', function(){
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Churchill" button.
+
+    console.log("This speech was written by " + speechesArray[0].author + " in " +  speechesArray[0].year + ".");
+
+    if(speechesArray[0].yearIsBCE === true) {
+      console.log("This speech took place before the common era.");
+    }
+    else {
+      console.log("This speech took place during the common era.");
+    }
+
+  // Compare this speech to the others to determine if it's the oldest or most recent speech on the page.   
+    if(speechesArray[0].year > speechesArray[1].year && speechesArray[0].year > speechesArray[2].year) {
+      console.log("This is the most recent speech on the page.");
+    }
+    
+    if(speechesArray[0].year < speechesArray[1].year && speechesArray[0].year < speechesArray[2].year) {
+      console.log("This is the oldest speech on the page.");
+    }
+
 });
 
 document.getElementById('BtnGhandi').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Ghandi" button.
+
+  console.log("This speech was written by " + speechesArray[1].author + " in " +  speechesArray[1].year + ".");
+
+    if(speechesArray[1].yearIsBCE === true) {
+      console.log("This speech took place before the common era.");
+    }
+    else {
+      console.log("This speech took place during the common era.");
+    }
+
+  // Compare this speech to the others to determine if it's the oldest or most recent speech on the page.   
+    if(speechesArray[1].year > speechesArray[0].year && speechesArray[1].year > speechesArray[2].year) {
+      console.log("This is the most recent speech on the page.");
+    }
+    
+    if(speechesArray[1].year < speechesArray[0].year && speechesArray[1].year < speechesArray[2].year) {
+      console.log("This is the oldest speech on the page.");
+    }
+
 });
 
 document.getElementById('BtnDemosthenes').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Demosthenes" button.
+
+  console.log("This speech was written by " + speechesArray[2].author + " in " +  speechesArray[2].year + ".");
+
+    if(speechesArray[2].yearIsBCE === true) {
+      console.log("This speech took place before the common era.");
+    }
+    else {
+      console.log("This speech took place during the common era.");
+    }
+
+  // Compare this speech to the others to determine if it's the oldest or most recent speech on the page.  
+    if(speechesArray[2].year > speechesArray[1].year && speechesArray[2].year > speechesArray[0].year) {
+      console.log("This is the most recent speech on the page.");
+    }
+    
+    if(speechesArray[2].year < speechesArray[1].year && speechesArray[2].year < speechesArray[0].year) {
+      console.log("This is the oldest speech on the page.");
+    }
 });
