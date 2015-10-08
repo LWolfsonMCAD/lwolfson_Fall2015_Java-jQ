7f87e93eac4808193d7263b32f7f2e10146054e9
Lisa Wolfson <lwolfson@mcad.edu>
2015-10-08 13:27:47 -0500

    Start of Assignment 7. Definitely need to clean up. Need to figure out proper order of JS created elements within event listeners (should they be global variables?). Need to work on appending more text to created H3, and hopefully moving sideNavHeadingText out of the if/else loop.

---
+++ week7/greatest_speeches/script.js
@@ -0,0 +1,138 @@
+//For readability and clarity it is good practice to declare variables at the beginning of the JS document if possible
+var churchillSpeech = {
+      'author': 'Churchill',
+      'year': 1940,
+      'yearIsBCE': false,
+      'authorAge': '66'
+    },
+    ghandiSpeech = {
+      'author': 'Ghandi',
+      'year': 1942,
+      'yearIsBCE': false,
+      'authorAge': '73'
+    },
+    demosthenesSpeech = {
+      'author': 'Demosthenes',
+      'year': 342,
+      'yearIsBCE': true,
+      'authorAge': '42'
+    },
+    speechesArray = [churchillSpeech, ghandiSpeech, demosthenesSpeech],
+    donatePrompt, amountToBeDonated;
+
+document.getElementById('BtnDonate').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Donate" button.
+  amountToBeDonated = window.prompt("How much would you like to donate?");
+
+  //H3 to be created with JS when "Donate" button is clicked, to thank the user for their donation.
+  var sideNavHeading = document.createElement("h3");
+//Not sure if it's right to split up the H3 creation...
+  document.getElementById("SideNav").appendChild(sideNavHeading);
+
+//Need to flush out the logic, hopefully move the sideNavHeadingText variable outside of this section.
+  if(amountToBeDonated < 100) {
+      var sideNavHeadingText = document.createTextNode("Thank you for your donation of $" + amountToBeDonated);
+  }
+  else if (amountToBeDonated >= 100) {
+
+  }
+  else {
+    document.getElementById("ConsoleDisplay").innerHTML = "Please enter a valid number.";
+  }
+
+  sideNavHeading.appendChild(sideNavHeadingText);
+
+});
+
+document.getElementById('BtnChurchill').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Churchill" button.
+
+  document.getElementById("ConsoleDisplay").innerHTML = 'This speech was written by ' + speechesArray[0].author + ' in ' + speechesArray[0].year + '. <br>';
+
+  if(speechesArray[0].yearIsBCE === true){
+    document.getElementById("ConsoleDisplay").innerHTML += 'This speech took place before the common era. <br>';
+  }else{
+    document.getElementById("ConsoleDisplay").innerHTML += 'This speech took place during the common era. <br>';
+  }
+
+  var oldest = speechesArray[0].year,
+      newest = speechesArray[0].year;
+
+  for(var i = 0; i < speechesArray.length; i++){
+    if(speechesArray[i].year < oldest){
+      oldest = speechesArray[i].year;
+    }
+    if(speechesArray[i].year > newest){
+      newest = speechesArray[i].year;
+    }
+  }
+
+  if(speechesArray[0].year === oldest){
+    document.getElementById("ConsoleDisplay").innerHTML += 'This is the oldest speech on the page. <br>';
+  }
+  if(speechesArray[0].year === newest){
+    document.getElementById("ConsoleDisplay").innerHTML += 'This is the most recent speech on the page. <br>';
+  }
+});
+
+document.getElementById('BtnGhandi').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Ghandi" button.
+
+  document.getElementById("ConsoleDisplay").innerHTML = 'This speech was written by ' + speechesArray[1].author + ' in ' + speechesArray[1].year + '. <br>';
+
+  if(speechesArray[1].yearIsBCE === true){
+    document.getElementById("ConsoleDisplay").innerHTML += 'This speech took place before the common era. <br>';
+  }else{
+    document.getElementById("ConsoleDisplay").innerHTML += 'This speech took place during the common era. <br>';
+  }
+
+  var oldest = speechesArray[0].year,
+      newest = speechesArray[0].year;
+
+  for(var i = 0; i < speechesArray.length; i++){
+    if(speechesArray[i].year < oldest){
+      oldest = speechesArray[i].year;
+    }
+    if(speechesArray[i].year > newest){
+      newest = speechesArray[i].year;
+    }
+  }
+
+  if(speechesArray[1].year === oldest){
+    document.getElementById("ConsoleDisplay").innerHTML += 'This is the oldest speech on the page. <br>';
+  }
+  if(speechesArray[1].year === newest){
+    document.getElementById("ConsoleDisplay").innerHTML += 'This is the most recent speech on the page. <br>';
+  }
+});
+
+document.getElementById('BtnDemosthenes').addEventListener('click', function(){
+  //Code in here executes when the user clicks the "Demosthenes" button.
+  
+  document.getElementById("ConsoleDisplay").innerHTML = 'This speech was written by ' + speechesArray[2].author + ' in ' + speechesArray[2].year + '. <br>';
+
+  if(speechesArray[2].yearIsBCE === true){
+    document.getElementById("ConsoleDisplay").innerHTML += 'This speech took place before the common era. <br>';
+  }else{
+    document.getElementById("ConsoleDisplay").innerHTML += 'This speech took place during the common era. <br>';
+  }
+
+  var oldest = speechesArray[0].year,
+      newest = speechesArray[0].year;
+
+  for(var i = 0; i < speechesArray.length; i++){
+    if(speechesArray[i].year < oldest){
+      oldest = speechesArray[i].year;
+    }
+    if(speechesArray[i].year > newest){
+      newest = speechesArray[i].year;
+    }
+  }
+
+  if(speechesArray[2].year === oldest){
+    document.getElementById("ConsoleDisplay").innerHTML += 'This is the oldest speech on the page. <br>';
+  }
+  if(speechesArray[2].year === newest){
+    document.getElementById("ConsoleDisplay").innerHTML += 'This is the most recent speech on the page. <br>';
+  }
+});
