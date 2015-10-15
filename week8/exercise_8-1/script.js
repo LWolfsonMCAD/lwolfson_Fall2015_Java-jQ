f710bc7970255ac632816598520c6ebbc08dd9d5
Lisa Wolfson <lwolfson@mcad.edu>
2015-10-15 14:24:39 -0500

    Comments

--- week8/exercise_8-1/script.js
+++ week8/exercise_8-1/script.js
@@ -4,11 +4,14 @@ var scrollToTopButtons = document.getElementsByClassName("btnScrollToTop");
 These functions can be used throughout the page.
 */
 
+//Scroll to the top of the page.
 function scrollToTopOfPage () {
      window.scrollTo (0,0);
 }
 
 
+
+//Whenever one of the "Scroll to Top" buttons are clicked, call the function to scroll up the window.
 for (var i = 0; i < scrollToTopButtons.length; i++) {
 	scrollToTopButtons[i].addEventListener('click', function() {
  		
