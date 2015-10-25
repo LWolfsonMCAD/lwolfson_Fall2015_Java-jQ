ffda69585686a1a91add33e53dccfb91ae72eae8
Lisa Wolfson <lwolfson@mcad.edu>
2015-10-25 15:14:07 -0500

    Changed my assignment…went with a variation of one of the week 8 exercises.

---
+++ week9/assignment_9/js/script.js
@@ -0,0 +1,21 @@
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    $("#quoteExplanation").hide();
+
+
+//First time the button is clicked, highlight the quote and display the explanation div.
+
+    $("#btnHighlightQuote").click(function() {
+    	var hamletQuote = document.getElementById("hamletQuote");
+    	var quoteExplanation = document.getElementById("quoteExplanation");
+		
+		$(hamletQuote).addClass("highlight");
+
+		$(quoteExplanation).show();
+
+    });
+
+});
