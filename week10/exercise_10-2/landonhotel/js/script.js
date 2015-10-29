de8e151eee13840dff926c14df7ac8147374ea1e
Lisa Wolfson <lwolfson@mcad.edu>
2015-10-29 14:06:47 -0500

    Need to wrap text in <a> and <li> tags...

--- week10/exercise_10-2/landonhotel/js/script.js
+++ week10/exercise_10-2/landonhotel/js/script.js
@@ -9,11 +9,44 @@ $( document ).ready(function() {
 
 
 //The following script creates a dynamic table of contents on the Specials page.
-	var citiesWithSpecialsList = $("div.content h2");
-	
-	$("div.content h1").after(citiesWithSpecialsList);
+	//var citiesWithSpecialsList = $("div.content h2");
 
-	$(citiesWithSpecialsList).wrap("<a> </a>").addClass("citiesList");
+	var citiesList = $( "div.content h1" ).after( "<ul id='citiesWithSpecials'></ul>" );
+
+	//var citiesWithSpecials = $("div.content h2").text();
+
+
+	var cities = [];
+
+	$("div.content h2").each(function() { 
+		cities.push( $(this).text() ) 
+	});
+
+	$("#citiesWithSpecials").append(cities);
+
+	// $.each(cities, function(index, item) {
+	// 	$(cities[index]).wrap("<li><a> </a></li>");
+	// });
+
+
+
+	// $.each(citiesWithSpecials, function() {
+	// 	$(this).wrap("<li><a> </a></li>");
+	// });
+
+	//console.log(citiesWithSpecials);
+
+	// $("ul#citiesList").append(function() {
+	// 	$.each(citiesWithSpecials, function() {
+	// 		$(this).wrap("<li><a> </a></li>");
+			
+	// 		//console.log(citiesWithSpecials[index].html = value);
+
+	// 		});
+	// 	});
+
+
+	//$(citiesWithSpecialsList).wrap("<a> </a>").addClass("citiesList");
 
 	//$("div.content h1").after(citiesWithSpecials);
 
