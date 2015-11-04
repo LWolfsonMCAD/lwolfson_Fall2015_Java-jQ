fb103ffc08f65abc776f04ae7a773b1e26902fb3
Lisa Wolfson <lwolfson@mcad.edu>
2015-11-03 21:47:42 -0600

    Week 11 files uploaded. Exercise 11.1 complete.

---
+++ week11/exercise11-1/landonhotel/js/script.js
@@ -0,0 +1,56 @@
+$( document ).ready(function() {
+
+/*******************************
+Meetings and Events Page
+********************************/
+	//Change the appearance of the div that the user is hovering over. 
+
+	$("div.content").on("mouseenter mouseleave", "div.event", function(event) {
+			$(this).toggleClass("highlightedEvent");
+
+			$("li", this).toggleClass("highlightedEvent");
+	});	
+
+/*******************************
+Restaurant-To-Room page
+********************************/
+	//The following script marks which restaurants in the list are veg-friendly for the Restaurant-To-Room page.
+	var vegRestaurants = $("li[data-type='veg']");
+
+	$(vegRestaurants).append("<span> (v)</span>");
+
+
+
+/*******************************
+Specials Page
+********************************/
+	//The following script creates a dynamic table of contents on the Specials page.
+
+	var 
+		citiesList = $( "div.content h1" ).after( "<ul id='citiesWithSpecials'></ul>" ),
+
+		cities = [];
+
+	// Populate the cities array to generate the list of cities that have specials, and give IDs to the H2 elements that the list will ink to.
+	$("div.content h2").each(function() { 
+		cities.push( $(this).text() )
+	});
+
+
+	$("div.content h2").each(function(i) {
+		$(this).attr("id", "city" + i);
+	});
+
+
+	//Wrap the cities array text in list item and anchor tags.
+
+	$.each(cities, function(index, value) {
+		var link = "#city" + index;
+		$("#citiesWithSpecials").append('<li><a href="'+link+'">' + value + ' </a></li>');
+	});
+
+
+});
+
+
+
