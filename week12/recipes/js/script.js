ca07670c259b5ca6e9da961585518e72a7744415
Lisa Wolfson <lwolfson@mcad.edu>
2015-11-15 17:18:03 -0600

    Got it to work…might try to get other buttons to not be faded on hover (not sure if I can do that with the current setup).

--- week12/recipes/js/script.js
+++ week12/recipes/js/script.js
@@ -1,98 +1,39 @@
-function heroImageLoop() {
-	var currentDiv = $("#hero div.current");
-	var nextDiv = ($(currentDiv).next().length > 0) ? $(currentDiv).next() : $("#hero div:first");
-
-	$( currentDiv )
-	    .queue(function() {
-	      $( this ).toggleClass( "current" ).toggleClass( "previous" ).dequeue();
-	    })
-	    .queue(function() {
-	    	$( nextDiv ).css({ opacity: 0.0 }).toggleClass( "current" ).animate({ opacity: 1.0 }, 1000, function() {
-	    		$( currentDiv ).removeClass( "previous" );
-	    	}).dequeue();
-	    });
-}
-
-//Solution from the video (I had the setInterval right, and my variables work...the video solution just does away with the .queue methods).
-
-// function heroImageLoop() {
-// 	var currentDiv = $("#hero div.current");
-// 	var nextDiv = $(currentDiv).next();
-
-// 	if (nextDiv.length === 0) {
-// 		nextDiv = $("#hero div:first");
-// 	}
-
-// 	currentDiv.removeClass("current").addClass("previous");
-// 	nextDiv.css({opacity: 0.0}).addClass("current")
-// 			.animate({opacity: 1.0}, 1000,
-// 					function() {
-// 						currentDiv.removeClass("previous");
-// 					});
-// }
-
-
-
 $( document ).ready(function() {
-/*******************************
-Homepage
-********************************/
-	//Cycle through images in #hero div.
+//Display the recipe that corresponds with the selected button.
 
-	setInterval(heroImageLoop, 3000);
+	$("ul#menu-main-menu li").click(function() {
+		var idName = $("a", this).data("divid");
+		var previousRecipe = $("#recipeDisplay div.currentRecipe");
 
+		$(this)
+				.css({opacity : 1.0})
+				.addClass("activeButton")
+				.queue(function() {
+	    			$(this).siblings()
+	    				.fadeTo("slow", 0.5)
+	    				.removeClass("activeButton")
+	    		}).dequeue();
 
-/*******************************
-Meetings and Events Page
-********************************/
-	//Change the appearance of the div that the user is hovering over. 
+		$(previousRecipe)
+				.toggleClass("currentRecipe")
+				.toggleClass("recipe");
 
-	$("div.content").on("mouseenter mouseleave", "div.event", function(event) {
-			$(this).toggleClass("highlightedEvent");
+		$("div#" + idName)
+				.css({opacity : 0.0})
+				.toggleClass("recipe")
+				.toggleClass("currentRecipe")
+				.animate({opacity : 1.0}, 1000);
 
-			$("li", this).toggleClass("highlightedEvent");
-	});	
-
-/*******************************
-Restaurant-To-Room page
-********************************/
-	//The following script marks which restaurants in the list are veg-friendly for the Restaurant-To-Room page.
-	var vegRestaurants = $("li[data-type='veg']");
-
-	$(vegRestaurants).append("<span> (v)</span>");
-
-
-
-/*******************************
-Specials Page
-********************************/
-	//The following script creates a dynamic table of contents on the Specials page.
-
-	var 
-		citiesList = $( "div.content h1" ).after( "<ul id='citiesWithSpecials'></ul>" ),
-
-		cities = [];
-
-	// Populate the cities array to generate the list of cities that have specials, and give IDs to the H2 elements that the list will ink to.
-	$("div.content h2").each(function() { 
-		cities.push( $(this).text() )
 	});
 
 
-	$("div.content h2").each(function(i) {
-		$(this).attr("id", "city" + i);
+//Change color of the button that the user is hovering over.
+
+	$("ul#menu-main-menu li").on("mouseenter mouseleave", function(event) {
+			$(this)
+				.toggleClass("currentButton");
+
 	});
-
-
-	//Wrap the cities array text in list item and anchor tags.
-
-	$.each(cities, function(index, value) {
-		var link = "#city" + index;
-		$("#citiesWithSpecials").append('<li><a href="'+link+'">' + value + ' </a></li>');
-	});
-
 
 });
-
-
 
