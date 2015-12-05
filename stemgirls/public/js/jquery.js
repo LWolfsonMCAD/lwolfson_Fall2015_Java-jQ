6cf593dee4684c9d16a154c7e662e06499ae41fe
Lisa Wolfson <lwolfson@mcad.edu>
2015-12-05 10:56:12 -0600

    If you click on "Astronaut" it now displays the corresponding Role Model names/titles, shows the chosen bio, and closes/resets when you click the button.
    Need a placeholder of sorts so the pages isn't just a list (maybe image and instructions to click?).
    
    Need role models under different menu options to see if it's all working properly.
    
    Need to do main menu drop down with query (currently it just uses css:hover).

--- stemgirls/public/js/jquery.js
+++ stemgirls/public/js/jquery.js
@@ -1,8 +1,68 @@
 
-
-
 $( document ).ready(function() {
+	var
+		roleModels = $("article.roleModel"),
+		references = $("section.references"),
+		roleModelBios = $("div.roleModelBio"),
+		fieldListChoices = $("div.fieldList"),
+		bioSection = $("section#roleModelBiographies"),
+		roleModelNames,
+		bioDivID,
+		bioDiv
+	;
 
+
+//Override default styles
+	$( bioSection ).hide();
+
+	$("section#roleModelFields div").removeClass("noJS");
+
+	$("div.fieldList a").hover(function() {
+		$(this).toggleClass("jQueryHover");
+	});
+
+	$("section#roleModelBiographies").height("100%");
+
+	$( roleModelBios )
+		.append("<button class='closeBio'>Close Biography</button>")
+		.add( references )
+		.hide();
+
+
+	// $("section#roleModelBiographies").css("min-height", "300px");
+
+
+
+
+
+//Display the H3 elements that correspond with the field chosen.
+	$( fieldListChoices ).find("a").click(function() {
+		bioDivID = $(this).attr("href"),
+		bioDiv = $("div" + bioDivID),
+		roleModelNames = $( bioDiv ).find("article h3");
+
+		$( bioSection ).show();
+
+	//Show the chosen woman's bio
+		$( roleModelNames ).click(function() {
+			$( roleModelNames ).not( $(this) ).hide();
+
+			$(this)
+				.next("div.roleModelBio")
+				.fadeIn(1000);
+
+			$( references ).show();
+		});
+
+		$("button.closeBio").click(function() {
+			$(this).closest("div.roleModelBio").hide();
+
+			$( references ).hide();
+
+			$( roleModelNames ).show();
+		});
+
+	});
 
 
 });
