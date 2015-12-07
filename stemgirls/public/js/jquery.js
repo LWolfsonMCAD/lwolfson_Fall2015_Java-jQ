b0f266efbdb5ae159678a94d65c38435d7df691e
Lisa Wolfson <lwolfson@mcad.edu>
2015-12-06 21:57:41 -0600

    Total disaster once I added another section (#physicistRoleModels), and tried changing the jquery code to work. Oh my word, I keep having to deal with a prevObject if I try clicking back on a Field List Choice that I already picked. I don't get it...

--- stemgirls/public/js/jquery.js
+++ stemgirls/public/js/jquery.js
@@ -1,8 +1,86 @@
+// $( document ).ready(function() {
+// 	var
+
+// //Role Model Page variables	
+// 		fieldListChoices = $("div.fieldList"),
+// 		bios = $("div.bios"),
+// 		roleModels = $("article.roleModel"),
+// 		biographyDisplay = $("div#biographyDisplay"),
+// 		roleModelBio = $("div.roleModelBio"),
+// 		roleModelDivID,
+// 		roleModelDiv,
+// 		roleModelNames,
+// 		roleModelReferences
+// 	;
+
+
+// //Override default styles
+// 	$( biographyDisplay ).height("100%");
+
+// 	$( "section#roleModelBiographies" ).hide();
+
+// 	$( fieldListChoices )
+// 		.removeClass("noJS")
+// 		.find("a")
+// 		.hover(function() {
+// 			$(this).toggleClass("jQueryHover");
+// 		});
+
+// 	$( roleModelBio )
+// 		.append("<button class='closeBio'>Close Biography</button>")
+// 		.hide();
+
+
+// //Display the H3 elements that correspond with the field chosen.
+// 	$( fieldListChoices ).find("a").on('click', function() {
+
+
+// 		// roleModelDivID = $(this).attr('href'),
+// 		// roleModelDiv = $("div" + roleModelDivID),
+// 		// roleModelNames = $( roleModelDiv ).find("article h3"),
+// 		// roleModelReferences = $( roleModelDiv ).find("div.references");
+
+// 		// $( biographyDisplay ).html(roleModelNames);
+
+// 		// console.log( roleModelNames );
+
+
+
+// //need to append clicked content to bioDisplay div and make sure it's only showing the proper names.		
+
+
+
+
+// //Show the chosen woman's bio, along with section references.
+// 		// $( roleModelNames ).click(function() {
+// 		// 	$( roleModelNames ).not( $(this) ).hide();
+
+// 		// 	$(this)
+// 		// 		.next( roleModelBio )
+// 		// 		.fadeIn(1000);
+
+// 		// 	$( roleModelReferences ).show();
+// 		// });
+
+// 		// $("button.closeBio").click(function() {
+// 		// 	$(this).closest( roleModelBio ).hide();
+
+// 		// 	$( roleModelReferences ).hide();
+
+// 		// 	$( roleModelNames ).show();
+// 		// });
+
+
+// 	});
+
+
+// });
+
 
 $( document ).ready(function() {
 	var
 		roleModels = $("article.roleModel"),
-		references = $("section.references"),
+		references = $("div.references"),
 		roleModelBios = $("div.roleModelBio"),
 		fieldListChoices = $("div.fieldList"),
 		bioSection = $("section#roleModelBiographies"),
