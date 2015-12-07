664703733fb022cc46867ba3e78d66b7d6d9319a
Lisa Wolfson <lwolfson@mcad.edu>
2015-12-07 08:41:15 -0600

    Figured it out…got current and previous bio divs to display. Need to fix the "active" field list choice and put in a better placeholder. Need to also check how it looks without jQuery.

--- stemgirls/public/js/jquery.js
+++ stemgirls/public/js/jquery.js
@@ -1,82 +1,3 @@
-// $( document ).ready(function() {
-// 	var
-
-// //Role Model Page variables	
-// 		fieldListChoices = $("div.fieldList"),
-// 		bios = $("div.bios"),
-// 		roleModels = $("article.roleModel"),
-// 		biographyDisplay = $("div#biographyDisplay"),
-// 		roleModelBio = $("div.roleModelBio"),
-// 		roleModelDivID,
-// 		roleModelDiv,
-// 		roleModelNames,
-// 		roleModelReferences
-// 	;
-
-
-// //Override default styles
-// 	$( biographyDisplay ).height("100%");
-
-// 	$( "section#roleModelBiographies" ).hide();
-
-// 	$( fieldListChoices )
-// 		.removeClass("noJS")
-// 		.find("a")
-// 		.hover(function() {
-// 			$(this).toggleClass("jQueryHover");
-// 		});
-
-// 	$( roleModelBio )
-// 		.append("<button class='closeBio'>Close Biography</button>")
-// 		.hide();
-
-
-// //Display the H3 elements that correspond with the field chosen.
-// 	$( fieldListChoices ).find("a").on('click', function() {
-
-
-// 		// roleModelDivID = $(this).attr('href'),
-// 		// roleModelDiv = $("div" + roleModelDivID),
-// 		// roleModelNames = $( roleModelDiv ).find("article h3"),
-// 		// roleModelReferences = $( roleModelDiv ).find("div.references");
-
-// 		// $( biographyDisplay ).html(roleModelNames);
-
-// 		// console.log( roleModelNames );
-
-
-
-// //need to append clicked content to bioDisplay div and make sure it's only showing the proper names.		
-
-
-
-
-// //Show the chosen woman's bio, along with section references.
-// 		// $( roleModelNames ).click(function() {
-// 		// 	$( roleModelNames ).not( $(this) ).hide();
-
-// 		// 	$(this)
-// 		// 		.next( roleModelBio )
-// 		// 		.fadeIn(1000);
-
-// 		// 	$( roleModelReferences ).show();
-// 		// });
-
-// 		// $("button.closeBio").click(function() {
-// 		// 	$(this).closest( roleModelBio ).hide();
-
-// 		// 	$( roleModelReferences ).hide();
-
-// 		// 	$( roleModelNames ).show();
-// 		// });
-
-
-// 	});
-
-
-// });
-
-
 $( document ).ready(function() {
 	var
 		roleModels = $("article.roleModel"),
@@ -86,12 +7,13 @@ $( document ).ready(function() {
 		bioSection = $("section#roleModelBiographies"),
 		roleModelNames,
 		bioDivID,
-		bioDiv
+		bioDiv,
+		idName,
+		previousBioDiv
 	;
 
 
 //Override default styles
-	$( bioSection ).hide();
 
 	$("section#roleModelFields div").removeClass("noJS");
 
@@ -99,7 +21,10 @@ $( document ).ready(function() {
 		$(this).toggleClass("jQueryHover");
 	});
 
-	$("section#roleModelBiographies").height("100%");
+	//$( bioSection ).height("100%");
+
+	$( bioSection ).find("div.bioDiv").addClass("inactiveBio");
+
 
 	$( roleModelBios )
 		.append("<button class='closeBio'>Close Biography</button>")
@@ -107,15 +32,22 @@ $( document ).ready(function() {
 		.hide();
 
 
-	// $("section#roleModelBiographies").css("min-height", "300px");
-
 //Display the H3 elements that correspond with the field chosen.
-	$( fieldListChoices ).find("a").click(function() {
-		bioDivID = $(this).attr("href"),
-		bioDiv = $("div" + bioDivID),
-		roleModelNames = $( bioDiv ).find("article h3");
+	$( fieldListChoices ).on('click', 'a', function() {
+		var 
+			idName = $(this).data("divid"),
+			currentBioDiv = $("div#" + idName),
+			previousBioDiv = $("#roleModelBiographies div.current"),
+			roleModelNames = $(currentBioDiv).find("article h3");	
 
-		$( bioSection ).show();
+		$(previousBioDiv)
+				.toggleClass("current")
+				.toggleClass("inactiveBio");
+
+		$(currentBioDiv)
+				.toggleClass("inactiveBio")
+				.toggleClass("current");
+
 
 	//Show the chosen woman's bio
 		$( roleModelNames ).click(function() {
